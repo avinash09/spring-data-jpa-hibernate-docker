84f5d3e2319943b306edf4df5ec49f04791efe2f
unknown <avinashjbs@gmail.com>
2022-01-08 19:57:10 +0530

    first commit

---
+++ src/test/java/com/techprimers/jpa/springdatajpahibernateexample/SpringDataJpaHibernateExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.jpa.springdatajpahibernateexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaHibernateExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
